15f98d37ba7d411cdab45aac546cf90a211de13c
Keeler Spear <keelerspear2@gmail.com>
2025-03-23 13:23:40 -0600

    Vastly improved function plotting

---
+++ src/Project0.java
@@ -0,0 +1,55 @@
+import java.util.function.Function;
+
+public class Project0 {
+
+    public static void main(String[] args) {
+
+        //1.1
+        System.out.println("1.1\n");
+        Matrix A = new Matrix(new double[][]{{1.0, 2.0}, {3.0, 4.0}});
+        System.out.println("Matrix A");
+        System.out.println(A);
+        System.out.println("A * A");
+        System.out.println(LinearAlgebra.multiplyMatrices(A, A));
+        System.out.println("A .* A");
+        System.out.println(LinearAlgebra.multiplyValues(A, A));
+        System.out.println("A^2");
+        System.out.println(LinearAlgebra.matrixPow(A, 2));
+        System.out.println("A / A");
+        System.out.println(LinearAlgebra.divideMatrices(A, A));
+        System.out.println("A ./ A");
+        System.out.println(LinearAlgebra.divideValues(A, A));
+
+        //1.2
+//        System.out.println("\n1.2\n");
+//        Matrix x = LinearAlgebra.linSpace(1, 10, 1);
+//        Matrix y = LinearAlgebra.valuesPow(x, 2);
+//
+//        PyChart.fnc(x, y, "fnc", "x", "y",  "title");
+
+        //1.3
+        System.out.println("\n1.3\n");
+        System.out.println("See Graphs");
+        //Chart I
+        Matrix xAxis = LinearAlgebra.linSpace(0, 20, 0.1);
+        Function<Double, Double> fnc = (t) -> Math.exp(-t / 4.0) * Math.sin(t);
+        PyChart.plotFunction(xAxis, fnc, "f(x)", "x", "y", "f(x) = exp(-x/4)*sin(x)");
+
+        //Chart II
+        Function<Double, Double> fnc2 = (t) -> Math.exp(-t / 4.0);
+
+        PyChart.plotTwoFunctions(xAxis, fnc, "f(x)", fnc2, "exp(-x/4)", "x", "y", "f(x) = exp(-x/4)*sin(x)");
+
+
+        //1.9
+        System.out.println("\n1.9\n");
+        System.out.println("Hello World");
+
+        //1.10
+        System.out.println("\n1.10\n");
+
+
+
+    }
+
+}
